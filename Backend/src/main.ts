ab4f0c132adc03278147c145f7f78af9dbc2734e
anthony <146010233+anthonyramosc@users.noreply.github.com>
2024-07-12 20:47:53 -0500

    comet

--- Backend/src/main.ts
+++ Backend/src/main.ts
@@ -3,6 +3,21 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+  .setTitle('Page API')
+  .setDescription('Landing page API description')
+  .setVersion('0.1')
+  .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  app.enableCors({
+    origin: 'http://localhost:3000',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
   await app.listen(3000);
 }
 bootstrap();
