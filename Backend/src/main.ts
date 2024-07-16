cee45efab322efc078944eff2be3af01d6c3f5cd
anthony <146010233+anthonyramosc@users.noreply.github.com>
2024-07-16 10:11:32 -0500

    commito

--- Backend/src/main.ts
+++ Backend/src/main.ts
@@ -4,15 +4,21 @@ import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  
+  app.enableCors({
+    origin: 'http://localhost:4200',
+  });
 
   // Configuración de Swagger
   const config = new DocumentBuilder()
-    .setTitle('Product API')
+    .setTitle('SIIV API')
     .setDescription('The product API description')
     .setVersion('1.0')
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
+
+
 
   await app.listen(3000);
 }
